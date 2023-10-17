7632c77c491977162967b3d3743143571733ec8a
Λ B H I N Λ V <ixd.abhi@gmail.com>
2023-10-18 01:19:25 +0530

    white theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,11 +3,6 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			fontFamily: {
-				sans: [
-				  'Poppins', 'sans-serif'
-				]
-			}, 
 			fontSize: {
 				'6xl': ['9rem', {
 				  lineHeight: '9rem',
